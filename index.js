46f29a336b4fe610c85c7b86b3bf735b0a387df9
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-04-27 06:17:20 -0400

    commit tree 8b814065ebc37bf688b92e59b47fd15fdb66352a

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+let random = Math.floor(Math.random()*2) + 1
+let num3 = 3
+let num4 = 12
+let mod = num4 / num3
+
+let max = Math.max(1, 20)
